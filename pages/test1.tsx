522ab5701a3ba7c74e800c452ed6cf270e32a46b
HadziqM <dimascrazz@gmail.com>
2022-10-04 16:13:28 +0700

    api go brrr

--- pages/test1.tsx
+++ pages/test1.tsx
@@ -9,25 +9,46 @@ export async function getServerSideProps() {
     props: { idk: user },
   };
 }
-
 interface Test1Props {}
-
 interface Test1State {}
-
 class Test1 extends React.Component<Test1Props, Test1State> {
   data: any;
+  interval: any;
   constructor(props: any) {
     super(props);
     this.data = props.idk[0];
   }
-  state = {};
+  state = { id: 0, username: "idk", password: "idk", sub: 0 };
+
+  testing = () => {
+    const data = this.data;
+    this.setState({
+      id: data.id,
+      username: data.username,
+      password: data.password,
+      sub: data.sub,
+    });
+  };
+  async getData() {
+    const res = await fetch("http://localhost:3000/api/hello");
+    const user = await res.json();
+    console.log(user);
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => this.getData(), 1000);
+  }
+  componentWillUnmount(): void {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <div>
-        <h1>{this.data.id}</h1>
-        <h1>{this.data.username}</h1>
-        <h1>{this.data.password}</h1>
-        <h1>{this.data.sub}</h1>
+        <button onClick={this.testing}>Test</button>
+        <h1>{this.state.id}</h1>
+        <h1>{this.state.username}</h1>
+        <h1>{this.state.password}</h1>
+        <h1>{this.state.sub}</h1>
       </div>
     );
   }
