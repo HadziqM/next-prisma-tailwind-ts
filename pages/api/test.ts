63d8c0b3d4b43c66d26a3234caf2b3816a6ea1a0
HadziqM <dimascrazz@gmail.com>
2022-10-11 15:59:36 +0700

    idk

---
+++ pages/api/test.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type Data = {
+  user:any
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const user = await prisma.users.findMany();
+  res.status(200).json({user : user[0]})
+}
